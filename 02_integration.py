66bd7745b2ebbfc2d1e9f1523fd1424489d96632
nortonajec <muotie@gmail.com>
2016-05-26 19:41:00 +0200

    Added lambdas

--- 02_integration.py
+++ 02_integration.py
@@ -4,16 +4,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def f(x):
-    return - x ** 5 + 2 * x ** 3 - x ** 2 + 4
-
-
-def integrate(x1, x2, maxmin_steps=100000, iterations=100000):
-    ymin = f(x1)
+def integrate(x1, x2, maxmin_steps=100000, iterations=100000, func=lambda x: x ** 2):
+    ymin = func(x1)
     ymax = ymin
     for i in xrange(maxmin_steps):
         x = x1 + (x2 - x1) * float(i) / maxmin_steps
-        y = f(x)
+        y = func(x)
         if y > ymax:
             ymax = y
         elif y < ymin:
@@ -24,22 +20,22 @@ def integrate(x1, x2, maxmin_steps=100000, iterations=100000):
     for j in xrange(iterations):
         x = x1 + (x2 - x1) * random.random()
         y = ymin + (ymax - ymin) * random.random()
-        if abs(y) <= abs(f(x)):
-            if f(x) > 0 and y > 0:
+        if abs(y) <= abs(func(x)):
+            if func(x) > 0 and y > 0:
                 points += 1
-            if f(x) < 0 and y < 0:
+            if func(x) < 0 and y < 0:
                 points -= 1
 
     integral = round(area * float(points) / iterations, 15)
     print 'Integral = %s' % integral
 
 
-def plot_integral(x1, x2, maxmin_steps=100000, iterations=100000):
-    ymin = f(x1)
+def plot_integral(x1, x2, maxmin_steps=100000, iterations=100000, func=lambda x: x ** 2):
+    ymin = func(x1)
     ymax = ymin
     for i in xrange(maxmin_steps):
         x = x1 + (x2 - x1) * float(i) / maxmin_steps
-        y = f(x)
+        y = func(x)
         if y > ymax:
             ymax = y
         elif y < ymin:
@@ -50,13 +46,13 @@ def plot_integral(x1, x2, maxmin_steps=100000, iterations=100000):
     for j in xrange(iterations):
         x = x1 + (x2 - x1) * random.random()
         y = ymin + (ymax - ymin) * random.random()
-        if abs(y) <= abs(f(x)):
-            if (f(x) > 0 and y > 0) or (f(x) < 0 and y < 0):
+        if abs(y) <= abs(func(x)):
+            if (func(x) > 0 and y > 0) or (func(x) < 0 and y < 0):
                 in_x.append(x)
                 in_y.append(y)
 
     x = np.linspace(x1, x2, 1000, endpoint=True)
-    y = f(x)
+    y = func(x)
     plt.style.use('ggplot')
     plt.plot(x, y, color='#ee2c2c', linewidth=1)
     plt.axhline(0, color='grey')
@@ -64,5 +60,5 @@ def plot_integral(x1, x2, maxmin_steps=100000, iterations=100000):
     plt.show()
 
 
-# integrate(0, 1, 1000000, 1000000)
-# plot_integral(-2, 2, 10000, 10000)
+# integrate(0, 1, 1000000, 1000000, func=lambda x: x ** 2)
+# plot_integral(-2, 2, 10000, 10000, func=lambda x: x ** 2)
